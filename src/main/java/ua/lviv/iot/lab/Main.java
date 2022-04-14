cef4179e74c1f874939bc04added087d926064fa
Oleh Yatskiv <92208702+Oleh325@users.noreply.github.com>
2022-04-14 17:39:50 +0300

    Lab 3 done and fixed .gitignore (#2)

--- src/main/java/ua/lviv/iot/lab/Main.java
+++ src/main/java/ua/lviv/iot/lab/Main.java
@@ -1,6 +1,10 @@
 package ua.lviv.iot.lab;
 
+import ua.lviv.iot.lab.manager.impl.CampingShopManager;
 import ua.lviv.iot.lab.model.*;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class Main {
 
@@ -10,6 +14,37 @@ public class Main {
         Lighter lighter = new Lighter("Oroblam", 6, "MBL", 400, 0.15f, true, 250);
         Tent tent = new Tent("Tent-S", 4, "TMC", 2500, 2, true, 6, 2.5f, 2, "ETI");
 
+        CampingShopManager shopManager = new CampingShopManager();
+
+        List<Good> goodsList = new LinkedList<>();
+        goodsList.add(blanket);
+        goodsList.add(flashlight);
+        goodsList.add(lighter);
+        goodsList.add(tent);
+
+        shopManager.addGoods(goodsList);
+        System.out.println("All goods: ");
+        goodsList.forEach(good -> {System.out.println(good);});
+        System.out.println("\n");
+
+        System.out.println("Sorted goods by weight in ascending order: ");
+        shopManager.sortGoodsByWeight(false);
+        System.out.println(shopManager.getGoodsList());
+        System.out.println("\n");
+        System.out.println("Sorted goods by weight in descending order: ");
+        shopManager.sortGoodsByWeight(true);
+        System.out.println(shopManager.getGoodsList());
+        System.out.println("\n");
+        System.out.println("Sorted goods by name in ascending order: ");
+        shopManager.sortGoodsByName(false);
+        System.out.println(shopManager.getGoodsList());
+        System.out.println("\n");
+        System.out.println("Sorted goods by name in descending order: ");
+        shopManager.sortGoodsByName(true);
+        System.out.println(shopManager.getGoodsList());
+        System.out.println("\n");
+        System.out.println("Find good by name Bree: ");
+        System.out.println(shopManager.findByName("Bree"));
 
     }
 
